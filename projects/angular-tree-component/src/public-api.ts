c7339168eb8bc9017178dff8eafa8696c640790a
Alex Kingman <alex.kingman@blackbaud.me>
2021-09-03 07:33:16 -0400

    partial ivy compilation

--- projects/angular-tree-component/src/public-api.ts
+++ projects/angular-tree-component/src/public-api.ts
@@ -1,1 +1,8 @@
 export * from './modules/angular-tree/angular-tree.module';
+
+// Components and directives must be exported to support Angular's "partial" Ivy compiler.
+// Obscure names are used to indicate types are not part of the public API.
+export { SkyAngularTreeWrapperComponent as λ1 } from './modules/angular-tree/angular-tree-wrapper.component';
+export { SkyAngularTreeNodeComponent as λ2 } from './modules/angular-tree/angular-tree-node.component';
+export { SkyAngularTreeContextMenuComponent as λ3 } from './modules/angular-tree/angular-tree-context-menu.component';
+export { SkyTreeViewToolbarComponent as λ4} from './modules/angular-tree/angular-tree-toolbar.component';
